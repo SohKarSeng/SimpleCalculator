4d32ca8152a1382ed979b08a1f0e625a10cb6258
Soh Kar Seng <kssoh1029@gmail.com>
2024-07-13 23:37:57 +0800

    Changed Label to TextField, Added Operation Button

--- src/main/java/com/example/simplecalculator/HelloController.java
+++ src/main/java/com/example/simplecalculator/HelloController.java
@@ -2,12 +2,12 @@ package com.example.simplecalculator;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 
 public class HelloController {
     @FXML
-    private Label inputText;
+    private TextField inputText;
 
     @FXML
     protected void calculatorButtonClick(ActionEvent event) {
