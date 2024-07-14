e90fd668d1cc56687b264e34b10b31fa16cd8b2a
Soh Kar Seng <kssoh1029@gmail.com>
2024-07-14 17:09:44 +0800

    Added Calculation Functions and Delete Function, Display operators and result in the screen.

--- src/main/java/com/example/simplecalculator/HelloController.java
+++ src/main/java/com/example/simplecalculator/HelloController.java
@@ -3,16 +3,24 @@ package com.example.simplecalculator;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 
 public class HelloController {
 
-    public String inputs = "";
+    private String inputs = "";
+    private String operators = "";
+    private float currentNumber = 0;
+    private float previousNumber = 0;
+    private float fresult = 0;
+    private String result = "";
     @FXML
     private TextField inputText;
+    @FXML
+    private Label operatorLabel;
 
     @FXML
-    protected void calculatorButtonClick(ActionEvent event) {
+    protected void calculatorNumberButtonClick(ActionEvent event) {
         Button clickedButton = (Button) event.getSource();
         String buttonText = clickedButton.getText();
         if (inputs.equals("")) {
@@ -22,5 +30,48 @@ public class HelloController {
             inputs += buttonText;
         }
         inputText.setText(inputs);
+    }
+
+    @FXML
+    protected void calculatorOperatorButtonClick(ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
+        String buttonText = clickedButton.getText();
+        operators = buttonText;
+        operatorLabel.setText(buttonText);
+        currentNumber = Float.parseFloat(inputs);
+        inputs = "";
+    }
+
+    @FXML
+    protected void calculatorEqualButtonClick() {
+        previousNumber = currentNumber;
+        currentNumber = Float.parseFloat(inputs);
+        if (operators.equals("+")){
+            result = Float.toString(previousNumber + currentNumber);
+            currentNumber = previousNumber + currentNumber;
+            inputText.setText(result);
+        }
+        if (operators.equals("-")){
+            result = Float.toString(previousNumber - currentNumber);
+            currentNumber = previousNumber - currentNumber;
+            inputText.setText(result);
+        }if (operators.equals("x")){
+            result = Float.toString(previousNumber * currentNumber);
+            currentNumber = previousNumber * currentNumber;
+            inputText.setText(result);
+        }
+        if (operators.equals("/")){
+            result = Float.toString(previousNumber / currentNumber);
+            currentNumber = previousNumber / currentNumber;
+            inputText.setText(result);
+        }
+    }
+
+    @FXML
+    protected void calculatorDelButtonClick() {
+        if (!inputs.equals("")) {
+            inputs = inputs.substring(0, inputs.length() - 1);
+            inputText.setText(inputs);
+        }
     }
 }
